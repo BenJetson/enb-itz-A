b7c33f86cf55c822a3169be47f897ca9d0ef0db2
Ben Godfrey <BenJetson@users.noreply.github.com>
2017-10-09 22:56:47 -0400

    Minor fixes.

--- startnb.sh
+++ startnb.sh
@@ -23,8 +23,9 @@ if [ $PLATFORM == 0 ]; then
 fi
 
 # verify that environment is sane
-CDENB_EXISTS=$(alias cdenb 2>/dev/null)
+CDENB_EXISTS=$(alias cdenb 2> /dev/null)
 ZIM_EXISTS=$(command -v zim)
+
 if ! [ $CDENB_EXISTS ]; then
     echo "ERROR: Couldn't find alias to cd to notebook path."
     return 1
