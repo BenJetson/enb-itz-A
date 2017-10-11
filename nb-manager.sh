9ca158fef8cde1f4c20b6cfe87d7139cabccf28a
Ben Godfrey <BenJetson@users.noreply.github.com>
2017-10-10 20:02:25 -0400

    Change variable names to match BASH conventions

--- nb-manager.sh
+++ nb-manager.sh
@@ -5,7 +5,7 @@
 #     alias 'cdenb' should cd to wherever the repository is stored
 
 # meta
-DEBUG=false
+debug=false
 
 spacer () {
     for i in {1..1}
@@ -21,46 +21,46 @@ section () {
 
 section "CHECKING ENVIRONMENT"
 # check which platform we're on
-OSTYPE=$(uname -s)
+os_type=$(uname -s)
 
-case "$OSTYPE" in 
-    Linux*)     PLATFORM=1;;
-    MINGW*)     PLATFORM=2;;
-    *)          PLATFORM=0;;
+case "$os_type" in 
+    Linux*)     platform=1;;
+    MINGW*)     platform=2;;
+    *)          platform=0;;
 esac 
 
 # verify that environment is sane
-CDENB_EXISTS=$(alias 'cdenb' 2>/dev/null)
-ZIM_PATH_EXISTS=$(command -v zim 2>/dev/null)
-ZIM_EXISTS=$(zim -v 1>/dev/null)
+cdenb_exists=$(alias 'cdenb' 2>/dev/null)
+zim_path_exists=$(command -v zim 2>/dev/null)
+zim_exists=$(zim -v 1>/dev/null)
 
-if [ $DEBUG = true ]; then 
+if [ $debug = true ]; then 
     spacer
-    echo "OS-TYPE: " $OSTYPE
-    echo "PLATFORM: " $PLATFORM
-    echo "CDENB-EXISTS: " $CDENB_EXISTS
-    echo "ZIM-EXISTS: " $ZIM_EXISTS
-    echo "ZIM-PATH-EXISTS: " $ZIM_PATH_EXISTS
+    echo "OS-TYPE: " $os_type
+    echo "platform: " $platform
+    echo "CDENB-EXISTS: " $cdenb_exists
+    echo "ZIM-EXISTS: " $zim_exists
+    echo "ZIM-PATH-EXISTS: " $zim_path_exists
     spacer
 fi
 
 # if platform unrecognized, then return to avoid breaking things
-if [ $PLATFORM == 0 ]; then
+if [ $platform == 0 ]; then
     echo "ERROR: Unrecognized platform -- this script may be incompatible"
     return 1
 fi
 
 # if environment isn't sane, display errors
 
-if [ -z "$CDENB_EXISTS" ]; then 
+if [ -z "$cdenb_exists" ]; then 
     echo "ERROR: Couldn't find alias to cd to notebook path."
     return 1
 fi
 
-if [ -z "$ZIM_PATH_EXISTS" ]; then
+if [ -z "$zim_path_exists" ]; then
     echo "ERROR: Couldn't find Zim Wiki by alias 'zim' or your path variable."
     return 1
-elif [ ! -z "$ZIM_EXISTS" ]; then
+elif [ ! -z "$zim_exists" ]; then
     echo "ERROR: Your path points to Zim Wiki, but no binary found!"
     return 1
 fi
@@ -80,12 +80,12 @@ git push
 
 # copy notebook template changes to system, if any
 section "COPY TEMPLATES"
-if [ $PLATFORM == 1 ]; then
+if [ $platform == 1 ]; then
     (
         set -e
         yes | cp -r ./temAAKplates ~/.local/share/zim/
     )
-elif [ $PLATFORM == 2 ]; then 
+elif [ $platform == 2 ]; then 
     yes | cp -r ./templates ~/AppData/Roaming/zim/data/zim/
 fi
 
